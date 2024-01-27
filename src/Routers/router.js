5ef3bf801f9c2fd40c05802d08c89b711cadf8ac
lucascampos04 <camposdlucasoli@gmail.com>
2024-01-27 19:00:05 -0300

    nova url

--- src/Routers/router.js
+++ src/Routers/router.js
@@ -5,7 +5,7 @@ export const Routers = () => {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/home" element={<HomePage/>}/>
+                <Route path="/" element={<HomePage/>}/>
             </Routes>
         </BrowserRouter>
     )
