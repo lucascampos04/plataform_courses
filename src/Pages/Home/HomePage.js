f0419d7e7ee0220e4de546d487f881d97f9f35f5
lucascampos04 <camposdlucasoli@gmail.com>
2024-01-27 17:17:18 -0300

    header

---
+++ src/Pages/Home/HomePage.js
@@ -0,0 +1,9 @@
+import { Header } from "../../Components/Headers/HeadersComponent"
+
+export const HomePage = () => {
+    return(
+        <>
+            <Header appName="Cursos.dev"/>
+        </>
+    )
+}
--- src/Routers/router.js
+++ src/Routers/router.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HomePage } from "../Pages/Home/HomePage"
 
 export const Routers = () => {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/"/>
+                <Route path="/home" element={<HomePage/>}/>
             </Routes>
         </BrowserRouter>
     )
