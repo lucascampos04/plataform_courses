f6c7d8ce88fa0f385af866c8bc60c3e4f840e983
lucascampos04 <camposdlucasoli@gmail.com>
2024-01-27 20:55:15 -0300

    e

--- src/Auth/Google/AuthGoogle.js
+++ src/Auth/Google/AuthGoogle.js
@@ -1,11 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { Navigate } from "react-router-dom";
-<<<<<<< HEAD
-import { AppConfigFirebase } from "../../../src/Service/Firebase/FirebaseConfig.js"
-=======
 import { AppConfigFirebase } from "../../../src/Service/Firebase/firebaseConfig";
->>>>>>> master
 
 const provider = new GoogleAuthProvider();
 
