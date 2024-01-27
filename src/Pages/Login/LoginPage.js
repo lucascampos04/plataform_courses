604cd2f2465f35bf98b0a945d693ff4ad835bc6f
lucascampos04 <camposdlucasoli@gmail.com>
2024-01-27 01:40:17 -0300

    Formulario atualizado

--- src/Pages/Login/LoginPage.js
+++ src/Pages/Login/LoginPage.js
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import "../../style/Login/LoginStyle.css"
 import { authContextGoogle } from "../../Context/AuthGoogle"
 import { Navigate } from "react-router"
@@ -13,38 +13,39 @@ export const LoginPage = () => {
     }
     return(
         <div className="container-fluid">
-            <div className="login-modal-container">
-                
-                <div className="form-row">
-                    <h1 className="modal-title-login">MiniCurso.dev</h1>
-                </div>
-
-                <div className="form-row">
-                    <input
-                        type="text"
-                        placeholder="username"
-                        className="form-group"
-                    />
-                </div>
-
-                <div className="form-row">
-                    <input
-                        type="text"
-                        placeholder="senha"
-                        className="form-group"
-                    />
-                </div>
-
-                <div className="form-row">
-                    <button className="form-row btn btn-success modal-button-login">
-                        Login
-                    </button>
-                </div>
-
-                <div className="form-row">
-                    <h3 className="modal-img-google" onClick={signInGoogle}>Google</h3>
-                </div>
-            </div>
+        <div className="login-modal-container">
+        
+        <div className="modal-container-title-login">
+            <h1 className="modal-title-login">MiniCurso.dev</h1>
         </div>
+        <div className="modal-container-inputs">
+        <div className = "form-row">
+            <input
+              type="text"
+              placeholder="Email"
+              className="form-control"
+            />
+          </div>
+    
+          <div className="form-row">
+            <input
+              type="text"
+              placeholder="Senha"
+              className="form-group form-control"
+            />
+          </div>
+    
+          <div className="form-row">
+            <button className="form-row btn btn-success modal-button-login">
+              Login
+            </button>
+          </div>
+    
+          <div className="form-row">
+            <h3 className="modal-img-google" onClick={signInGoogle}>Google</h3>
+          </div>
+        </div> 
+        </div>
+      </div>
     )
 }
