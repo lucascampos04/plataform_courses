e207423b5269b012b85034d19a266bc69e685e2c
lucascampos04 <camposdlucasoli@gmail.com>
2024-01-28 13:46:08 -0300

    Login e logout funcionado

--- src/Auth/EmailAndPassword/AuthDefault.js
+++ src/Auth/EmailAndPassword/AuthDefault.js
@@ -2,14 +2,21 @@ import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 
-export const login = async (email, password) => {
+export const login = async (email, password, navigate) => {
   try {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    if (!emailRegex.test(email)){
+      throw new Error("Formato de email invalido")
+    }
+
     const auth = getAuth();
     await signInWithEmailAndPassword(auth, email, password);
   } catch (error) {
-    console.error("Erro no login:", error.message);
+    throw error
   }
 };
 
@@ -17,8 +24,16 @@ export const register = async (email, password) => {
   try {
     const auth = getAuth();
     await createUserWithEmailAndPassword(auth, email, password);
-    console.log("Cadastrado com sucesso");
   } catch (error) {
-    console.error("Erro no cadastro:", error.message);
+    throw new Error("Erro no cadasto ", error.message)
   }
 };
+
+export const logout = async () => {
+  try {
+    const auth = getAuth()
+    await signOut(auth)
+  } catch (e) {
+    throw new e("Error ao desconectar ", e.message)
+  }
+}
--- src/Routers/router.js
+++ src/Routers/router.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { HomePage } from "../Pages/Home/HomePage"
 import { LoginPage } from "../Pages/Login/LoginPage"
 import { RegisterPage } from "../Pages/Register/RegisterPage"
+import { CentralPages } from "../Pages/central/CentralPage"
 
 export const Routers = () => {
     return(
@@ -11,6 +12,7 @@ export const Routers = () => {
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="/central" element={<CentralPages/>}/>
             </Routes>
         </BrowserRouter>
     )
